f79b1bd10d74ce03cd37dea258b3024c67dab70f
ciijay99 <cjahanwa@gmail.com>
2021-11-30 15:15:37 +0000

    Final Changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,14 +4,14 @@ from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
     def test_for_null_inputEN(self):
-        self.assertEqual(english_to_french(None), None)
-    def test1(self):
+        self.assertNotEqual(english_to_french(''), 'Bonjour')
+    def test_translation(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test_for_null_inputFR(self):
-        self.assertEqual(french_to_english(None), None)
-    def test2(self):
+        self.assertNotEqual(french_to_english(''), 'Hello')
+    def test_translation(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -20,27 +20,33 @@ language_translator.set_service_url(url)
 
 def english_to_french(english_text):
     """ Function To Translate English To French """
-    while True:
-        try:
+    try:
+        if len(english_text) >= 1:
             translation = language_translator.translate(english_text, model_id='en-fr').get_result()
             trans_list = translation["translations"]
             trans_dict = trans_list[0]
             french_text = trans_dict['translation']
             return french_text
-        except ValueError:
-            return None
+        else:
+            french_text = ""
+            return french_text
+    except ValueError:
+        return None
 
 def french_to_english(french_text):
-    '''  Function To Translate FrenchToEnglish '''
-    while True:
-        try:
+    """ Function To Translate French To English """
+    try:
+        if len(french_text) >= 1:
             translation = language_translator.translate(french_text, model_id='fr-en').get_result()
             trans_list = translation["translations"]
             trans_dict = trans_list[0]
             english_text = trans_dict['translation']
             return english_text
-        except ValueError:
-            return None
+        else:
+            english_text = ""
+            return english_text
+    except ValueError:
+        return None
 
 #print(englishToFrench('Hello'))
 #print(frenchToEnglish('Bonjour'))
